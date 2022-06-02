6ea7bf6a490ab840780bbbb45234cb433c32e22c
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-06-02 07:13:12 +0530

    commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//import routes
+const postRoutes = require('./route/routes');
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//route middleware
+app.use(postRoutes);
+
+const port = 8000;
+const DB_URL = 'mongodb+srv://pavi:pavi123@library.unfbj.mongodb.net/library-app?retryWrites=true&w=majority';
+mongoose.connect(DB_URL , {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+    .then(()=>{
+        console.log('DB connected');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+app.listen(port , ()=>{
+    console.log(`app is running on ${port}`);
+});
+
