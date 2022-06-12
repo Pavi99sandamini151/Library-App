715915a9a611b4b8b2f8a56d1732ce2467c1a4a6
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-06-12 21:26:13 +0530

    commit

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,10 +1,12 @@
 import React , {Component} from "react";
 import photo from './images/about us1.jpg';
+import Footer from "./Footer";
 
 
 export default class AboutUs extends Component{
     render(){
       return(
+        <div>
           <div class="about-us-container">
             <div className="left">
               <img className="photo" src={photo}></img>
@@ -12,8 +14,19 @@ export default class AboutUs extends Component{
             <div className="right">
                <h1 class="heading">About Us</h1>
                <p class="paragraph">loream loream</p>
+                <div className="card-container">
+                  <div class="card">
+                      <div class="card-body">
+                      <a href="#">This is some text within a card body.</a>
+                    </div>
+                  </div>
+               </div>
             </div>
+
+            
           </div>
+          <Footer></Footer> 
+        </div>  
       )
     }
 }
